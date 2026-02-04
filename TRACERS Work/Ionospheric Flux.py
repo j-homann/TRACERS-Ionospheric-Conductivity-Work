b42fa6d02830110e40592cefb6932cbc6db85b34
j-homann <jason-homann@uiowa.edu>
2026-02-04 13:30:48 -0600

    Commit 2

---
+++ TRACERS Work/Ionospheric Flux.py
@@ -0,0 +1,24 @@
+# Imports
+import spaceToolsLib as stl
+import numpy as np
+from scipy.integrate import simpson
+import tqdm
+
+# Loading the Data File
+file = rf"C:\Users\jason\OneDrive\Documents\TRACERS\l3\ts2_l3_ace_pitch-angle-dist-pre_20251231_v0.2.0.cdf"
+data_dict = stl.loadDictFromFile(file)
+print(data_dict.keys())
+
+# Getting the Data
+differential_energy_flux = data_dict['ts2_l3_ace_pitch_def'][0]
+Energy = data_dict['ts2_l3_ace_energy'][0]
+pitch_angle = data_dict['ts2_l3_ace_pitch_angle'][0]
+Epoch = data_dict['Epoch'][0]
+
+flux = []
+# Calculating Energy Flux into the Ionosphere
+for i in Epoch:
+    for j in pitch_angle:
+        I_Energy = simpson(differential_energy_flux[0,:,0], Energy)
+        I_Theta= simpson(I_Energy*np.sin(pitch_angle), pitch_angle)
+        flux.append(2*np.pi*I_Energy*I_Theta)
--- TRACERS Work/main.py
+++ TRACERS Work/main.py
@@ -1,6 +1,6 @@
 # Imports
-import spaceToolsLib as stl
 import numpy as np
+import spaceToolsLib as stl
 from scipy.integrate import simpson
 
 # Loading the Data File
@@ -14,8 +14,3 @@ print(np.shape(data_dict['ts2_l2_ace_def'][0]))
 # phi = data_dict['VARIABLE NAME HERE'][0]
 # theta = data_dict['VARIABLE NAME HERE'][0]
 # differential_energy_flux = data_dict[]
-
-# # Calculating Energy Flux into the Ionosphere
-# I_theta = simpson(np.cos, theta, axis=0)
-# I_phi   = simpson(I_theta, phi, axis=0)
-# I_total = simpson(I_phi, E)
