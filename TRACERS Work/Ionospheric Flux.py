e9484b1fb0f627e444b2cead5f8924e8caa73269
j-homann <jason-homann@uiowa.edu>
2026-02-10 14:01:35 -0600

    Commit 2

--- TRACERS Work/Ionospheric Flux.py
+++ TRACERS Work/Ionospheric Flux.py
@@ -15,10 +15,18 @@ Energy = data_dict['ts2_l3_ace_energy'][0]
 pitch_angle = data_dict['ts2_l3_ace_pitch_angle'][0]
 Epoch = data_dict['Epoch'][0]
 
+# Calculating Energy Flux
 flux = []
-# Calculating Energy Flux into the Ionosphere
-for i in Epoch:
-    for j in pitch_angle:
-        I_Energy = simpson(differential_energy_flux[0,:,0], Energy)
+for i in tqdm.tqdm((range(len(Epoch)))):
+    for j in range(len(pitch_angle)):
+        I_Energy = simpson(differential_energy_flux[i,:,j], Energy)
         I_Theta= simpson(I_Energy*np.sin(pitch_angle), pitch_angle)
         flux.append(2*np.pi*I_Energy*I_Theta)
+
+# Calculating ???
+energy_flux = []
+for i in tqdm.tqdm((range(len(Epoch)))):
+    for j in range(len(pitch_angle)):
+        I_Energy = simpson(Energy*differential_energy_flux[i,:,j], Energy)
+        I_Theta= simpson(I_Energy*np.sin(pitch_angle), pitch_angle)
+        energy_flux.append(2*np.pi*I_Energy*I_Theta)
